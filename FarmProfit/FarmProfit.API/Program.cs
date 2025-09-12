d8328428e22f90016f24bcb717f048a9325c513a
Vladislav Cotlearov <vcotlearov@users.noreply.github.com>
2025-09-12 21:16:35 +0300

    added Auth0

--- FarmProfit/FarmProfit.API/Program.cs
+++ FarmProfit/FarmProfit.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 namespace FarmProfit.API
 {
     public class Program
@@ -10,7 +13,28 @@ namespace FarmProfit.API
 
             builder.Services.AddControllers();
 
-            var app = builder.Build();
+
+			builder.Services.AddAuthentication(options =>
+				{
+					options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				})
+				.AddJwtBearer(options =>
+				{
+					options.Authority = "https://dev-iqadq0gbmsvx3bju.us.auth0.com/";
+					options.Audience = "https://farm-profit-webapp.azurewebsites.net";
+
+					// Optional: validate additional parameters
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuer = true
+					};
+				});
+
+			// Add Authorization if needed
+			builder.Services.AddAuthorization();
+
+			var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
