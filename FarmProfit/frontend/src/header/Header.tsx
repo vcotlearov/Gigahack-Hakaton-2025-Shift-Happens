4b5999eb6deac5d1e4857a269b578d0d8458ea35
Pavel Prodan <prodanpavel94@gmail.com>
2025-09-14 01:15:28 +0300

    remove hamburger menu from header

--- FarmProfit/frontend/src/header/Header.tsx
+++ FarmProfit/frontend/src/header/Header.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Avatar, Box, MenuItem } from "@mui/material"
 import { Logo } from "../logo/Logo"
-import { BurgerMenu } from "../icons/BurgerMenu"
 import { useAuth0 } from "@auth0/auth0-react";
 
 
@@ -10,9 +9,7 @@ export const Header = () => {
         <AppBar position="fixed" sx={{ zIndex: (t) => t.zIndex.drawer + 1, borderRadius: 0, backgroundColor: 'white', color: 'black' }}>
             <Box display="flex" justifyContent="space-between" alignItems="center" sx={{ padding: '16px 24px' }}>
                 <Box display="flex" alignItems="center" gap={3}>
-                    <BurgerMenu />
                     <Logo />
-
                 </Box>
                 <Box display='flex' alignItems='center' gap={1}>
                     <Avatar>JD</Avatar>
